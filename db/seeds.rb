dffad2c156ee2de3235c3a594b90caa0df7c1a3e
serpentz <absalomcherinet@gmail.com>
2018-11-14 10:41:06 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+require 'pry'
 User.create(name: "sample user")
-
-Post.create(text: "Meme 1", url: "test1")
-Post.create(text: "Meme 2", url: "test2")
-
 
 Category.create(name: "girlfriend")
 Category.create(name: "minds")
 
-
 CategoryPost.create(post_id: 1, category_id: 1)
 CategoryPost.create(post_id: 2, category_id: 2)
 
-
 SelectedMeme.create( user_id: 1, post_id: 1)
 SelectedMeme.create( user_id: 1, post_id: 2)
+
+data = JSON.parse(RestClient.get("https://www.reddit.com/top/.json?count=90").body)
+
+
+data['data']['children'].each do |x|
+
+	 Post.find_or_create_by( text: x['data']['title'], url: x['data']['url'] )
+	end
+  
+

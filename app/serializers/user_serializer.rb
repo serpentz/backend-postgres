d25cdc75d397f3d43c2aa8164337b5a9598ef869
Nicholas Smith <nico.ehs05@gmail.com>
2018-11-16 15:10:38 +0000

    fix serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :selected
   def selected
-      user.posts.map{|el| el.id}
+      self.object.posts.map{|el| el.id}
   end
 end
